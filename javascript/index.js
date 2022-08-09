3d4615666e9191bd5798e4effc07451789e4c010
Franciane Pires <franpp22@gmail.com>
2022-08-09 17:40:22 -0300

    realizando a função de formatacao da moeda

--- javascript/index.js
+++ javascript/index.js
@@ -1,35 +1,52 @@
-// lista de transação 
-const transacao = [ 
-    {
-        tipo: '-',
-        mercadoria: 'blusa',
-        valor: 15.00,
 
-    },
-    {
-        tipo: '+' ,
-        mercadoria: 'calça',
-        valor: 50.00,
 
+//campo de valor formatado para receber somente numero
+function formatarMoeda() { 
+    var elemento = document.getElementById('valor');
+    var valor = elemento.value;
+
+    valor = valor + ''; 
+    valor = parseInt(valor.replace(/[\D]+/g, '')); 
+    valor = valor + ''; 
+    valor = valor.replace(/([0-9]{2})$/g, ",$1"); 
+
+    if (valor.length > 6) { 
+        valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2"); 
     }
-]
 
-for(operacao in transacao) {
-    document.querySelector('table.tabela thead').innerHTML +=
-        `
-        <tr>
-            <td>
-            ${(transacao[operacao] == 'Compra' ? '-' : '+')}
-            </td>
-            <td>
-            ${transacao[operacao].mercadoria}
-            </td>
-            <td>
-            ${transacao[operacao].valor}
-            </td>
-        </tr>
-    
-        ` 
+    elemento.value = valor; 
+    if (valor == 'NaN') elemento.value = ''; 
 }
 
 
+// const lista = [
+//     {
+//         tipoTransacao: '-',
+//         nomeMercadoria: 'blusa',
+//         valorMercadoria: 1
+//     }
+// ] 
+
+
+function adicionarTransacao() { 
+    const tipoTransacao = document.getElementById('tipo-transacao'); 
+    const nomeTransacao = document.getElementById('nome-transacao'); 
+    const valorTransacao = document.getElementById('valor-transacao');
+
+    let dados = localStorage.getItem('dadosTransacao');
+
+    if(dados == null) { 
+        localStorage.setItem('dadosTransacao', '[]');
+        dados = [];
+    }
+
+    const registroTransacao = { 
+        selecao: tipoTransacao.value,
+        nome: nomeTransacao.value,
+        valor: valorTransacao.value
+    }
+
+    dados.push(registroTransacao);
+
+    localStorage.setItem('dadosTransacao', JSON.stringify(dados));
+}
