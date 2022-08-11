a84fa4ff712c5ff4d6f7aa4346eb3b4217af0d3b
Franciane Pires <franpp22@gmail.com>
2022-08-11 18:40:12 -0300

    formatacao da moeda, adicionando transacao no localstorage com o push, botão de limpar extrato e testando desenha tabela.

--- javascript/index.js
+++ javascript/index.js
@@ -1,52 +1,79 @@
 
+//permitir somente numeros
+function formatarMoeda() {
+    var elemento = document.querySelector('#valor');
+        var valor = elemento.value;
 
-//campo de valor formatado para receber somente numero
-function formatarMoeda() { 
-    var elemento = document.getElementById('valor');
-    var valor = elemento.value;
+        valor = valor + ''; 
+        valor = parseInt(valor.replace(/[\D]+/g, '')); 
+        valor = valor + ''; 
+        valor = valor.replace(/([0-9]{2})$/g, ",$1"); 
 
-    valor = valor + ''; 
-    valor = parseInt(valor.replace(/[\D]+/g, '')); 
-    valor = valor + ''; 
-    valor = valor.replace(/([0-9]{2})$/g, ",$1"); 
-
-    if (valor.length > 6) { 
+        if (valor.length > 6) { 
         valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2"); 
+        }
+
+        elemento.value = valor; 
+        if (valor == 'NaN') elemento.value = '';
+}
+
+function adicionarTransacao(e) { 
+    e.preventDefault();    
+
+    //usando localStorage para armazenar dados 
+    var extratoCru = localStorage.getItem('extrato')
+
+    if (extratoCru == null) { 
+        var extrato = JSON.parse(extratoCru)
+    } else { 
+        var extrato = [];
     }
 
-    elemento.value = valor; 
-    if (valor == 'NaN') elemento.value = ''; 
+    // está indo pro localstorage, mas não aparece na tabela 
+    extrato.push({
+        selecionarTransacao: e.target.elements['selecionar-transacao'].value,
+        nomeMercadoria: e.target.elements['nome-mercadoria'].value,
+        valor: e.target.elements['valor'].value
+    });
+
+    localStorage.setItem('extrato', JSON.stringify(extrato));
+
+    //tem que alterar na tabela //
+    console.log(extrato)
 }
 
 
-// const lista = [
-//     {
-//         tipoTransacao: '-',
-//         nomeMercadoria: 'blusa',
-//         valorMercadoria: 1
-//     }
-// ] 
+// não está aparecendo no extrato//
 
 
-function adicionarTransacao() { 
-    const tipoTransacao = document.getElementById('tipo-transacao'); 
-    const nomeTransacao = document.getElementById('nome-transacao'); 
-    const valorTransacao = document.getElementById('valor-transacao');
+//contatenação de array para realizar a limpeza do extrato, usando limpar extrato 
+function limparExtrato() {
 
-    let dados = localStorage.getItem('dadosTransacao');
-
-    if(dados == null) { 
-        localStorage.setItem('dadosTransacao', '[]');
-        dados = [];
-    }
-
-    const registroTransacao = { 
-        selecao: tipoTransacao.value,
-        nome: nomeTransacao.value,
-        valor: valorTransacao.value
-    }
-
-    dados.push(registroTransacao);
-
-    localStorage.setItem('dadosTransacao', JSON.stringify(dados));
+    linhasExistentes = [...document.querySelectorAll('table.tabela tbody .conteudo-dinamico')];
+    linhasExistentes.forEach(element => {
+    element.remove()
+});
 }
+
+//chamando alista estatica para a tabela 
+function desenhaTabela() { 
+    for (operacao in extrato) {
+    document.querySelector('table.tabela tbody').innerHTML +=
+    `
+        <tr class="conteudo-dinamico">
+            <td>
+                ${elements.extrato.selecionarTransacao == 'Compra' ? '-' : '+'}
+            </td>
+            <td>
+                ${elements.extrato.nomeMercadoria}
+            </td>
+            <td>
+                ${formatarMoeda(valor)}
+            </td>
+        </td>
+    ` 
+}
+}
+// como chamar, pois está dando undef
+//desenhaTabela()
+
