8a4f33b2fc3462da9de643595b8f807b4abbfa18
Franciane Pires <franpp22@gmail.com>
2022-09-20 09:56:55 -0300

    tipo de operação +/-

--- javascript/index.js
+++ javascript/index.js
@@ -26,32 +26,34 @@ if (produtosCru != null) {
 
 //loop 
 function reescrevaLista() { 
-    for (extrato in produtos) {
-        document.querySelector('.extrato-tabela').innerHTML +=`
+    document.querySelector(".extrato-tabela tbody").innerHTML = ""
+
+    //selecionar +/-
+    for(let i = 0; i < produtos.length; i ++) { 
+        var tipoTransacao = "-"
+
+        if (produtos[i].tipo == "Compra") { 
+            tipoTransacao = "+"
+        }
+        document.querySelector('.extrato-tabela tbody').innerHTML +=`
         <tr class="borda-acima" id="uma-linha">
-            <td class="text-right">${produtos[extrato].tipo}</td>
-            <td>${produtos[extrato].nome}</div>
+            <td class="text-right">${tipoTransacao}</td>
+            <td>${produtos[i].nome}</div>
             </td>
-            <td class="text-right">R$ ${produtos[extrato].valor}
+            <td class="text-right">R$ ${produtos[i].valor}
             </td>
         </tr>`
     }
+
+    
+    
 }
 
 function enviarFormulario(e) { 
     e.preventDefault()
-    // não permiti letra 
-    for (i in e.target.elements['valor-mercadoria'].value) { 
-        if (["0","1","2","3","4","5","6","7","8","9"].indexOf(e.target.elements['valor-mercadoria'].value[i]) == -1) {
-            alert('Apenas números são permitidos no campo de valor!')
-            return false
-        } else { 
-            
-        }
-    }
+    console.log(e.target.elements)
 
-
-
+    console.log(e)
 
     var produtosCru = localStorage.getItem('produtos')
     if (produtosCru != null) { 
