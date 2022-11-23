76a643abb4ff29e84f920e34faef6fe61216d6da
Franciane <franpp22@gmail.com>
2022-11-23 12:40:45 -0300

    criação de array, inicio da funçao de envio de formulario e desenha tabela

--- javascript/index.js
+++ javascript/index.js
@@ -7,16 +7,30 @@ let produto = [
 ]
 // console.log(produto)
 
-// function desenhaTabela() { 
+
+function enviarFormulario(e) { 
+    e.preventDefault()
+
+    produto.push({ 
+        tipo: e.target.elements['selecionar-transacao'].selectedIndex == 1 ? '-' : '+', 
+        mercadoria: e.target.elements["nome-mercadoria"].value,
+        valor: e.target.elements["valor-mercadoria"].value,
+    })
+
+}
+
+
+function desenhaTabela() { 
 
     for(transacao in produto) { 
-       document.querySelector("table.extrato-tabela thead").innerHTML += `
-        <tr>
+       document.querySelector("table.extrato-tabela thead").innerHTML +=
+       ` <tr>
             <td>${produto[transacao].tipo}</td>
             <td>${produto[transacao].mercadoria}</td>
             <td>${produto[transacao].valor}</td>
-        </tr>
-    `
-
+        </tr> `
     }
+}
+
+desenhaTabela()
         
