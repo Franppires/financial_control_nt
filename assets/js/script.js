0933f0dcc2859c30e5e9e196fe1ac40388e1e310
Franciane <franpp22@gmail.com>
2022-12-13 10:52:40 -0300

    ajuste na mascara e  variavel

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,8 @@ var produto = [] // inicia vazio
 
 var produtoCru = localStorage.getItem('produto')  // armazenamento no localStorage
 if (produtoCru != null) { 
-    var produto = JSON.parse(produtoCru)
-} else { 
-    var produto = []
-}
+    produto = JSON.parse(produtoCru)
+} 
 
 // função para enviar dados 
 function enviarTransacao(e) { 
@@ -26,15 +24,13 @@ function enviarTransacao(e) {
     //verifica se só tem numero e considera somente os numeros
     numero = /[0-9]+/g
     if (!numero.test(e.target.elements['valor-mercadoria'].value)) { 
-        alert('Apenas números são permitidos no campo de valor!')
+        alert('Campo de valor não preenchido corretamente, apenas números são permitidos!')
         return false
     }
     produto.push({     // envia dados para localStorage
         tipo: e.target.elements['selecionar-transacao'].selectedIndex == 1 ? '-' : '+', 
         mercadoria: e.target.elements['nome-mercadoria'].value,
-        valor: e.target.elements['valor-mercadoria'].value
-        .replace('.', '')
-        .replace(',', '.'),
+        valor: e.target.elements['valor-mercadoria'].value        
     })
 
     localStorage.setItem('produto', JSON.stringify(produto)) //salvando extrato no localstorage
@@ -42,15 +38,15 @@ function enviarTransacao(e) {
 }
 
 function mascara(e) { 
-    valor = document.getElementById('valor-mercadoria')
+    let valor = document.getElementById('valor-mercadoria')
     valor.value = valor.value.replace(/[^0-9]+/g, '') // pegar somente números
 
     if (valor.value.length <= 2) {
         valor.value = ('000' + valor.value).substring(-3) // centavos
     }
-    
+
     valor.value = valor.value.replace(/([0-9]{2})$/g, '.$1') // milhar
-    valor.value = parseFloat(valor.value).toLocaleString('pt-BR') // transforma em string
+    valor.value = parseFloat(valor.value).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) // transforma em string
 }
 
 function limparExtrato() { 
@@ -58,29 +54,18 @@ function limparExtrato() {
 
     if(confirm(msg) == true) { 
         produto = []
-        document.querySelector('tfoot').innerHTML = ''
+        resultadoTotal = ''
         localStorage.clear()
         desenhaTabela()
     }
 }
 
 function desenhaTabela() { 
-    document.querySelector('tbody').innerHTML = '' // limpa extrato
-    resultado = ''
+    let extrato = document.querySelector('tbody')
+    extrato.innerHTML = ''// limpa extrato
 
-    // começo tabela
-    document.querySelector('thead').innerHTML = `
-    <tr>
-        <th></th>
-        <th style="text-align:start;">Mercadoria</th>
-        <th style="text-align:end;">Ações</th>
-        <th style="text-align:end;">Valor</th>
-        
-    </tr>`
-
-    // nenhuma transação
-    if (produto.length == 0) { 
-        document.querySelector('tbody').innerHTML = `
+    if (produto.length == 0) { // nenhuma transação
+        extrato.innerHTML = `
         <tr>
             <td style="border: none;"></td>
             <td style="border: none; font-size:24px; text-align:center;"><strong>Nenhuma transação cadastrada!</strong></td>
@@ -91,65 +76,47 @@ function desenhaTabela() {
 
     let total = 0 // total do valor de mercadoria
 
-    console.log(document.querySelectorAll('tbody .conteudo-dinamico'))
-
+    console.log(total)
     for(transacao in produto) { 
-
-        if (produto[transacao].tipo == '+') { //seleciona tipo de operação
-            total += parseFloat((produto[transacao].valor))
-        } else { 
-            total -= parseFloat((produto[transacao].valor))
-        }
-
         let dinheiro = produto[transacao].valor // coloca campo dentro de variavel para conversão
+        let tipo = produto[transacao].tipo //tipo de operação positiva ou negativa 
 
-        // extraato da tabela
-        document.querySelector('tbody').innerHTML += ` 
+        console.log(total = dinheiro * (tipo == '+' ? 1 : -1)) //calculo soma e subtração
+
+        // extrato da tabela
+        extrato.innerHTML += ` 
         <tr class="conteudo-dinamico">
-            <td>${produto[transacao].tipo}</td>
+            <td>${tipo}</td>
             <td style="width: 50%;">${produto[transacao].mercadoria}</td>
             <td style="text-align:end;">
-                <button onclick="excluirLinha(${transacao})"> <strong> Excluir </strong></button> 
-            </td>
-            <td style="text-align:end;"> ${parseFloat(dinheiro).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</td>
-            
-        </tr> 
-        `
+                <button onclick="excluirLinha"${transacao}> <strong> Excluir </strong></button> </td>
+            <td style="text-align:end;"> ${dinheiro}</td>
+        </tr> `
     }
 
-    // resultado positivo ou negativo
-    resultado = document.querySelector('tfoot').innerHTML 
+    let resultado = document.querySelector('tfoot div')  //resultado lucro ou prejuizo
+    total > 0 ? resultado = '[LUCRO]' : resultado = '[PREJUIZO]'
 
-    if (total > 0) { 
-        resultado = '[LUCRO]'
-    } else if (total < 0) { 
-        resultado = '[PREJUIZO]'
-    }
+    let resultadoTotal = document.querySelector('tfoot') //escrever total
+    resultadoTotal.innerHTML = '' 
 
-    if (resultado == 0) { 
-        document.querySelector('tfoot').innerHTML = ''
-    } else { 
-        document.querySelector('tfoot').innerHTML = `
+    if (total !== 0) {  
+        resultadoTotal.innerHTML = `
         <tr style="border-top-style: double">
             <td style="border: none;"></td>
             <td style="border: none;"><strong>Total</strong></td>
             <td style="border: none;"></td>
-            <td style="border: none; text-align:end;"><strong> ${parseFloat(total).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</strong>
-                <div style="font-size:10px; text-align:end;" > ${resultado}</div>
-            </td>
-        </tr>
-         `
-    }
+            <td style="border: none; text-align:end;"><strong> ${total}</strong>
+                <div style="font-size:10px; text-align:end;"> ${resultado}</div>  </td>
+        </tr> `
+     } 
+
 }   
 
-function excluirLinha(e) { 
+function excluirLinha(e) { //excluir linha
     produto.splice(e, 1); 
     desenhaTabela();
     localStorage.setItem('produto', JSON.stringify(produto))
-
-    if (produto.length == 0) { 
-        document.querySelector('tfoot').innerHTML = ''
-    }
 }
 
 desenhaTabela()
@@ -159,20 +126,19 @@ desenhaTabela()
 let menu = document.querySelector('.nav')
 let menuItens = document.querySelector('.menu-texto')
 let abrirMenu = document.querySelector('.menu-hamburguer')
-let fechaMenu = document.querySelector('.fechar')
 let menuIcon = document.querySelector('.menu-icon')
 
 function toggleMenu() { 
     if(menu.classList.contains('ativo')) { 
         menu.classList.remove('ativo')
-        fechaMenu.style.display = 'none'
+        abrirMenu.style.display = 'none'
         menuIcon.style.display = 'block'
     } else { 
         menu.classList.add('ativo')
-        fechaMenu.style.display = 'block'
+
+        abrirMenu.style.display = 'block'
         menuIcon.style.display = 'none'
     }
 }
 
 abrirMenu.addEventListener('click', toggleMenu)
-fechaMenu.addEventListener('click', toggleMenu)
